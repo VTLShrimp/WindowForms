6f9208a595bb4a4bc83f229d7edcf7611ea147f2
Vũ Thanh Lâm <126549415+VTLShrimp@users.noreply.github.com>
2024-03-25 13:39:51 +0700

    Add files via upload

---
+++ Test/Login.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Test
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.ShowPass = new System.Windows.Forms.CheckBox();
+            this.LinkForgotPass = new System.Windows.Forms.LinkLabel();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserText = new System.Windows.Forms.TextBox();
+            this.PassWordTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ForgetPasswordLink = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(72, 187);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(72, 259);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // ShowPass
+            // 
+            this.ShowPass.AutoSize = true;
+            this.ShowPass.CausesValidation = false;
+            this.ShowPass.Location = new System.Drawing.Point(144, 310);
+            this.ShowPass.Name = "ShowPass";
+            this.ShowPass.Size = new System.Drawing.Size(125, 20);
+            this.ShowPass.TabIndex = 3;
+            this.ShowPass.Text = "Show Password";
+            this.ShowPass.UseVisualStyleBackColor = true;
+            this.ShowPass.CheckedChanged += new System.EventHandler(this.ShowPass_CheckedChanged);
+            // 
+            // LinkForgotPass
+            // 
+            this.LinkForgotPass.Location = new System.Drawing.Point(0, 0);
+            this.LinkForgotPass.Name = "LinkForgotPass";
+            this.LinkForgotPass.Size = new System.Drawing.Size(100, 23);
+            this.LinkForgotPass.TabIndex = 14;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LoginButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LoginButton.Location = new System.Drawing.Point(130, 366);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(133, 41);
+            this.LoginButton.TabIndex = 4;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CancelButton.Location = new System.Drawing.Point(340, 366);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(133, 41);
+            this.CancelButton.TabIndex = 5;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = false;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(588, 94);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 7;
+            // 
+            // UserText
+            // 
+            this.UserText.BackColor = System.Drawing.SystemColors.Window;
+            this.UserText.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserText.ForeColor = System.Drawing.Color.Gray;
+            this.UserText.Location = new System.Drawing.Point(144, 200);
+            this.UserText.Name = "UserText";
+            this.UserText.Size = new System.Drawing.Size(342, 32);
+            this.UserText.TabIndex = 1;
+            this.UserText.Text = "Enter your name";
+            this.UserText.TextChanged += new System.EventHandler(this.UserText_TextChanged);
+            this.UserText.Enter += new System.EventHandler(this.UserText_Enter);
+            this.UserText.Leave += new System.EventHandler(this.UserText_Leave);
+            // 
+            // PassWordTextBox
+            // 
+            this.PassWordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PassWordTextBox.ForeColor = System.Drawing.Color.Gray;
+            this.PassWordTextBox.Location = new System.Drawing.Point(144, 272);
+            this.PassWordTextBox.Name = "PassWordTextBox";
+            this.PassWordTextBox.Size = new System.Drawing.Size(342, 32);
+            this.PassWordTextBox.TabIndex = 2;
+            this.PassWordTextBox.Text = "Enter your password";
+            this.PassWordTextBox.TextChanged += new System.EventHandler(this.PassWordTextBox_TextChanged);
+            this.PassWordTextBox.Enter += new System.EventHandler(this.PassWordTextBox_Enter);
+            this.PassWordTextBox.Leave += new System.EventHandler(this.PassWordTextBox_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(88, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(420, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Enter your Username and Password";
+            // 
+            // ForgetPasswordLink
+            // 
+            this.ForgetPasswordLink.AutoSize = true;
+            this.ForgetPasswordLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForgetPasswordLink.Location = new System.Drawing.Point(127, 431);
+            this.ForgetPasswordLink.Name = "ForgetPasswordLink";
+            this.ForgetPasswordLink.Size = new System.Drawing.Size(136, 20);
+            this.ForgetPasswordLink.TabIndex = 6;
+            this.ForgetPasswordLink.TabStop = true;
+            this.ForgetPasswordLink.Text = "Forgot Password";
+            this.ForgetPasswordLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.ForgetPasswordLink_LinkClicked);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(588, 512);
+            this.Controls.Add(this.ForgetPasswordLink);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PassWordTextBox);
+            this.Controls.Add(this.UserText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.LinkForgotPass);
+            this.Controls.Add(this.ShowPass);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "Login";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox ShowPass;
+        private System.Windows.Forms.LinkLabel LinkForgotPass;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UserText;
+        private System.Windows.Forms.TextBox PassWordTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel ForgetPasswordLink;
+    }
+}
+
---
+++ Test/Login.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+
+    public partial class Login : Form
+    {    
+        
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            if (UserText.Text == "Enter your name")
+            {
+                MessageBox.Show("Chua nhap tai khoan", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (PassWordTextBox.Text == "Enter your password")
+            {
+                MessageBox.Show("Chua nhap mat khau", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection(@"Data Source=LAMLMAO;Initial Catalog=User_Management;Integrated Security=True;");
+                    con.Open();
+                    string UserName = UserText.Text;
+                    string Password = PassWordTextBox.Text;
+                    string sql = "select * from User_Info where UserName = '" + UserName + "' and UserPassword = '" + Password + "'";
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read() == true)
+                    {
+                        reader.Close();
+                        string UserType = "select * from User_Info where UserName = '" + UserName + "' and UserType = 'admin'";
+                        SqlCommand cmd2 = new SqlCommand(UserType, con);
+                        SqlDataReader CheckAdmin = cmd2.ExecuteReader();
+                        if (CheckAdmin.Read() == true)
+                        {
+                            MessageBox.Show("Dang nhap admin thanh cong", "Thong bao",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                            CheckAdmin.Close();
+                            AdminForm adminForm = new AdminForm();
+                            adminForm.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Dang nhap user thanh cong", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            CheckAdmin.Close();
+                            UserForm userForm = new UserForm();
+                            userForm.Show();
+                            this.Hide();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tai khoan hoac mat khau khong dung", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Co loi xay ra", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Warning );
+                }
+            }          
+        }
+        private void PassWordTextBox_Enter(object sender, EventArgs e)
+        {
+            if (PassWordTextBox.Text == "Enter your password")
+            {
+                PassWordTextBox.Text = null;
+                PassWordTextBox.PasswordChar = '*';
+            }
+            PassWordTextBox.ForeColor = Color.Black;
+
+            if (ShowPass.Checked == true)
+            {
+                PassWordTextBox.PasswordChar = '\0';
+            }
+        }
+
+        private void PassWordTextBox_Leave(object sender, EventArgs e)
+        {
+            if (PassWordTextBox.Text == "")
+            {
+                PassWordTextBox.Text = "Enter your password";
+                PassWordTextBox.PasswordChar = '\0';
+            }
+            PassWordTextBox.ForeColor = Color.Gray;
+        }
+
+        private void UserText_Enter(object sender, EventArgs e)
+        {
+            if (UserText.Text == "Enter your name")
+            {
+                UserText.Text = "";
+            }
+            UserText.ForeColor = Color.Black;
+        }
+
+        private void UserText_Leave(object sender, EventArgs e)
+        {
+            if (UserText.Text == "")
+            {
+                UserText.Text = "Enter your name";
+            }
+            UserText.ForeColor = Color.Gray;
+        }
+
+     
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ForgetPasswordLink_Click(object sender, EventArgs e)
+        {
+            ResetForm resetForm = new ResetForm();         
+            resetForm.Show();  
+            this.Hide();
+           
+        }
+
+        private void ShowPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if(ShowPass.Checked)
+            {
+                PassWordTextBox.PasswordChar = '\0';
+            }
+            else
+            {
+                if (PassWordTextBox.Text != "Enter your password")
+                {
+                    PassWordTextBox.PasswordChar = '*';                   
+                }
+            }
+        }
+
+        private void PassWordTextBox_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void UserText_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void ForgetPasswordLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ResetForm reset = new ResetForm();
+            reset.Show();   
+        }
+    }
+}
